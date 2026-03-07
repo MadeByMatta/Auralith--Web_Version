c0c8cea87bb38987a0d1fe9619f2255167b83cd9
MadeByMatta <luiscachero2005@gmail.com>
2026-03-07 21:07:49 +0100

    Solucion error de servidor interno

--- config/db.js
+++ config/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2/promise');
 
 // Crear pool de conexiones
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root', // Cambiar según la configuración local de XAMPP/MySQL
-    password: '', // Normalmente vacío en XAMPP o instalar el correspondiente
-    database: 'auralith_db',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root', // Cambiar según la configuración local de XAMPP/MySQL
+    password: process.env.DB_PASSWORD || '', // Normalmente vacío en XAMPP o instalar el correspondiente
+    database: process.env.DB_NAME || 'auralith_db',
+    port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
